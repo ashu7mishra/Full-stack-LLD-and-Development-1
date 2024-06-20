ce2c3dc7fa2fa53087d437dfc9358d8351c3a978
Ashutosh <ashu7mishra@gmail.com>
2024-06-20 05:55:44 +0530

    progress bar complete

--- Javascript/LectureFullStackLLD&ProjectsJavaScript-10-UICaseStudies-1/ProgressBar/index.js
+++ Javascript/LectureFullStackLLD&ProjectsJavaScript-10-UICaseStudies-1/ProgressBar/index.js
@@ -2,15 +2,33 @@ const progressRef = document.querySelector('.progress');
 const startRef = document.querySelector('.start');
 const stopRef = document.querySelector('.stop');
 const resetRef = document.querySelector('.reset');
+// let intervalRef;
+let animationFrameRef;
+
+// function renderProgress() {
+//     intervalRef = setInterval(() => {
+//         let newValue = getProgressValue() + 1;
+//         setProgressValue(newValue);
+//         if (newValue >= 100){
+//             clearInterval(intervalRef);
+//         }
+//     }, 1000);
+// }
+
+// function stopProgress(){
+//     clearInterval(intervalRef);
+// }
 
 function renderProgress() {
-    const intervalRef = setInterval(() => {
-        let newValue = getProgressValue() + 1;
-        setProgressValue(newValue);
-        if (newValue >= 100){
-            clearInterval(intervalRef);
-        }
-    }, 1000);
+    let newValue = getProgressValue() + 1;
+    setProgressValue(newValue);
+    if (newValue <= 100){
+        animationFrameRef = requestAnimationFrame(renderProgress);
+    }
+}
+
+function stopProgress(){
+    cancelAnimationFrame(animationFrameRef);
 }
 
 function getProgressValue() {    
@@ -21,4 +39,23 @@ function setProgressValue(value) {
     progressRef.style.width = `${value}%`;
 }
 
-renderProgress();
+startRef.addEventListener('click', function(){
+    renderProgress();
+    startRef.setAttribute('disabled', true);
+    stopRef.removeAttribute('disabled');
+
+});
+
+stopRef.addEventListener('click', function() {
+    stopProgress();
+    startRef.removeAttribute('disabled');
+    stopRef.setAttribute('disabled', true);
+});
+
+resetRef.addEventListener('click', function(){
+    stopProgress();
+    progressRef.style.width = '0%';
+    startRef.removeAttribute('disabled');
+    stopRef.setAttribute('disabled', true);
+});
+
