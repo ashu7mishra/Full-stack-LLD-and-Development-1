f5090a20fc5bce1f2a5717c82c1b2b97ed0861b3
Ashutosh <ashu7mishra@gmail.com>
2024-06-22 07:47:46 +0530

    calender complete

--- Javascript/FullStackLLD&ProjectsJavaScript-11-UICaseStudies-2/Calender/index.js
+++ Javascript/FullStackLLD&ProjectsJavaScript-11-UICaseStudies-2/Calender/index.js
@@ -8,7 +8,7 @@ const todayButtonRef = document.querySelector('.today-section button');
 
 const MONTHS = [
     'Jan',    
-    'Feb',    
+    'Feb',
     'Mar',    
     'Apr',    
     'May',    
@@ -24,17 +24,95 @@ const MONTHS = [
 const todayDate = new Date();
 
 function generateMonthOptions(){
-    MONTHS.forEach(month =>{
-        const createOption = document.createElement('option');
-        createOption.value = idx;
-        createOption.innerText = month;
+    const monthFragment = document.createDocumentFragment();
+    MONTHS.forEach((month, idx) =>{
+        const createMonthOption = document.createElement('option');
+        createMonthOption.value = idx;
+        createMonthOption.innerText = month;
+        createMonthOption.selected = todayDate.getMonth() === idx;
+        monthFragment.appendChild(createMonthOption);
     })
+    monthSelectRef.appendChild(monthFragment);
 }
 
 function generateYearOptions(){
+    const currentYear = todayDate.getFullYear();
+    const yearFragment = document.createDocumentFragment();
+    for(let i = currentYear-100; i <= currentYear+100; i++){
+        const createYearOption = document.createElement('option');
+        createYearOption.value = i;
+        createYearOption.innerText = i;
+        createYearOption.selected = todayDate.getFullYear() === i;
+        yearFragment.appendChild(createYearOption);
+    }
+    YearSelectRef.appendChild(yearFragment);
 
 }
 
-function generateDays(){
+function generateDays(month, year){
+    datesRef.innerHTML = '';
+    const startDay = new Date(year, month,1).getDay();
+    const totalDaysInMonth = new Date(year, month+1,0).getDate();
+    const datesFragment = document.createDocumentFragment();
+
+    for (let i=0; i<startDay; i++){
+        const spanRef = document.createElement('span');
+        datesFragment.appendChild(spanRef);
+    }
+
+    for (let j=1; j<=totalDaysInMonth; j++){
+        const spanRef = document.createElement('span');
+        spanRef.classList.add('date');
+        spanRef.setAttribute('data-id', j);
+        spanRef.innerText = j;
+        datesFragment.appendChild(spanRef);
+    }
+    datesRef.appendChild(datesFragment);
 
 }
+
+generateMonthOptions();
+generateYearOptions();
+generateDays(6, 2024);
+
+monthSelectRef.addEventListener('change', function(ev){
+    const selectedMonth = ev.target.value;
+    const selectedYear = YearSelectRef.value;
+    generateDays(selectedMonth, selectedYear);
+});
+
+YearSelectRef.addEventListener('change', function(ev){
+    const selectedYear = ev.target.value;
+    const selectedMonth = monthSelectRef.value;
+    generateDays(selectedMonth, selectedYear);
+});
+
+nextButtonRef.addEventListener('click', function(ev){
+    let selectedYear = +YearSelectRef.value;
+    let selectedMonth = +monthSelectRef.value;
+    if (selectedMonth === 11){
+        YearSelectRef.value = selectedYear + 1;
+        monthSelectRef.value = 0;
+    }else{
+        monthSelectRef.value = selectedMonth + 1;
+    }
+    generateDays(monthSelectRef.value, YearSelectRef.value);
+});
+
+prevButtonRef.addEventListener('click', function(ev){
+    let selectedYear = +YearSelectRef.value;
+    let selectedMonth = +monthSelectRef.value;
+    if (selectedMonth === 0){
+        YearSelectRef.value = selectedYear - 1;
+        monthSelectRef.value = 11;
+    }else{
+        monthSelectRef.value = selectedMonth - 1;
+    }
+    generateDays(monthSelectRef.value, YearSelectRef.value);
+});
+
+todayButtonRef.addEventListener('click', function(ev){
+    YearSelectRef.value = todayDate.getFullYear();
+    monthSelectRef.value = todayDate.getMonth();
+    generateDays(monthSelectRef.value, YearSelectRef.value);
+})
