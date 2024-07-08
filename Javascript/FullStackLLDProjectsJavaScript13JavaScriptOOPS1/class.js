8a16d32bf6fe7479154e33698c27615a920193b7
Ashutosh <ashu7mishra@gmail.com>
2024-07-08 10:47:58 +0530

    oops1 complete

--- Javascript/FullStackLLDProjectsJavaScript13JavaScriptOOPS1/class.js
+++ Javascript/FullStackLLDProjectsJavaScript13JavaScriptOOPS1/class.js
@@ -1,21 +1,53 @@
-let person = {
-    name:'Ashu',
-    course: 'FullStack',
-    getName: function(){
+// let person = {
+//     name:'Ashu',
+//     course: 'FullStack',
+//     getName: function(){
+//         return this.name;
+//     }
+// }
+
+// console.log(person.name);
+// console.log(person.getName());
+
+// person2 = {
+//     name: 'Krish',
+//     course: 'FullStack',
+//     getName: function(){
+//         return this.name;
+//     }
+// }
+
+// console.log(person2.name);
+// console.log(person2.getName());
+
+// constructor function
+// function Person(name, course){
+//     this.name = name;
+//     this.course = course;
+//     this.getName = function(){
+//         return this.name;
+//     }
+//     this.getCourse = function(){
+//         return this.course;
+//     }
+// }
+
+// const Mohsin = new Person('Mohsin', 'FullStack');
+// const Swati = new Person('Swati', 'FullStack');
+// console.log(Mohsin.getName());
+// console.log(Swati.getName());
+
+
+function Car(name, color){
+    this.name = name;
+    this.color = color;
+    this.getName = function(){
         return this.name;
     }
+    return this;
 }
 
-console.log(person.name);
-console.log(person.getName());
-
-person2 = {
-    name: 'Krish',
-    course: 'FullStack',
-    getName: function(){
-        return this.name;
-    }
-}
-
-console.log(person2.name);
-console.log(person2.getName());
+const BMW = new Car('BMW', 'Red');
+console.log(BMW);
+console.log(global.color);
+console.log(BMW.color);
