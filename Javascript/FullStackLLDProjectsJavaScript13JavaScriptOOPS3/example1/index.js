d18dff99841236fabd4a1a9fa2318644b56f9e1b
Ashutosh <ashu7mishra@gmail.com>
2024-07-11 20:03:31 +0530

    oops3 continue

--- Javascript/FullStackLLDProjectsJavaScript13JavaScriptOOPS3/example1/index.js
+++ Javascript/FullStackLLDProjectsJavaScript13JavaScriptOOPS3/example1/index.js
@@ -26,3 +26,24 @@ class Car1{
 
 const BMW1 = new Car1('A1', '2023');
 const AUDI1 = new Car1('X1', '2022');
+
+
+class SuperCar extends Car{
+    constructor(price, model, make){
+        super(model, make);
+        this.price = price;
+        this.totalPrice = function(tax){
+            return tax+this.price;
+        }
+    }
+
+    getDetail(){
+        return `Price of model ${this.model} is ${this.price}`
+    }
+}
+
+const LuxuryBMW = new SuperCar(2000000, 'A1', '2023');
+const LuxuryAudi = new SuperCar(1000000, 'A2', '2023');
+
+console.log(LuxuryBMW);
+console.log(LuxuryAudi);
