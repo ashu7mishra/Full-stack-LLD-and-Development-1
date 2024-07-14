f63def8b136f98411f65a5fea7c1e9ca12a5ed5a
Ashutosh <ashu7mishra@gmail.com>
2024-07-14 08:59:06 +0530

    Asynchronous Programming 1 complete

--- Javascript/FullStackLLDProjectsJavaScript16AsynchronousProgramming1/example3.js
+++ Javascript/FullStackLLDProjectsJavaScript16AsynchronousProgramming1/example3.js
@@ -1,29 +1,39 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// console.log('Start');
-// fs.readFile('f1.txt', readFileCallback1);
-// console.log('End');
+console.log('Start');
+fs.readFile('f1.txt', readFileCallback1);
+console.log('End');
 
-// function readFileCallback1(err, data){
-//     if(err){
-//         console.log(err);
-//     }else{
-//         console.log('File 1 Data > ', data);
-//     }
+function readFileCallback1(err, data){
+    if(err){
+        console.log(err);
+    }else{
+        fs.readFile('f2.txt', readFileCallback2)
+        console.log('File 1 Data > ', data);
+    }
+}
+
+function readFileCallback2(err, data){
+    if(err){
+        console.log(err);
+    }else{
+        // fs.readFile('f2.txt', readFileCallback2)
+        console.log('File 2 Data > ', data);
+    }
+}
+
+// function Outer(){
+//     Inner();
+//     console.log('Outer');
 // }
 
-function Outer(){
-    Inner();
-    console.log('Outer');
-}
+// function Inner(){
+//     Child();
+//     console.log('Inner');
+// }
 
-function Inner(){
-    Child();
-    console.log('Inner');
-}
+// function Child(){
+//     console.log('Child');
+// }
 
-function Child(){
-    console.log('Child');
-}
-
-Outer();
+// Outer();
