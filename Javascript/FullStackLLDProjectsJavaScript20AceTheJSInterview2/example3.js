de02ed361a79cb1fc385d0bf229bd5daec3ae4dd
Ashutosh <ashu7mishra@gmail.com>
2024-07-23 07:57:32 +0530

    example3 complete

---
+++ Javascript/FullStackLLDProjectsJavaScript20AceTheJSInterview2/example3.js
@@ -0,0 +1,36 @@
+// Q1. Implement a function setCancellableInterval, that acts like
+// setInterval but instead of returning a timer ID, it returns a 
+// function that when called, cancels the interval.The setCancellableInterval 
+// function should have the exact same signature as setInterval
+
+// setCancellableInterval(callback);
+// setCancellableInterval(callback, delay);
+
+
+function setCancellableInterval(cb, delay) {
+    // Implementation
+    const timerId = setInterval(cb, delay);
+    return function(){
+        clearInterval(timerId);
+    }
+}
+
+let i = 0;
+const cancel1 = setCancellableInterval(() => {
+    i++;
+    console.log('First : ',i)
+}, 1000);
+
+setTimeout(() => {
+    cancel1();
+}, 5000);
+
+let j = 0;
+const cancel2 = setCancellableInterval(() => {
+    j++;
+    console.log('Second : ',j)
+}, 1000);
+
+setTimeout(() => {
+    cancel2();
+}, 10000);
